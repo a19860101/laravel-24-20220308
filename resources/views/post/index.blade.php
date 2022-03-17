5f696155695773cae06380b9c441cc134d355872
Zac <a19860101@gmail.com>
2022-03-17 20:10:43 +0800

    20220317

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,14 +9,18 @@
         @foreach($posts as $post)
         <div class="col-10 border rounded my-3 p-3">
             <h2>{{$post->title}}</h2>
+            <small>{{$post->created_at}}</small>
             <div class="my-3">
                 {{$post->content}}
             </div>
             <div>
                 <a href="{{route('post.show',['post'=>$post->id])}}" class="btn btn-primary">繼續閱讀</a>
             </div>
+            @php
+            Carbon\Carbon::setLocale('zh_TW');
+            @endphp
             <div class="float-end">
-                {{$post->updated_at}}
+                {{Carbon\Carbon::parse($post->updated_at)->diffForHumans()}}
             </div>
         </div>
 
