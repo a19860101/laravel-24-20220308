386c9566b8b5500439bb920a2dcf15c638b30224
Zac <a19860101@gmail.com>
2022-03-17 19:22:40 +0800

    20220317

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,34 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <h1>文章列表</h1>
-    <a href="/post/create">建立文章</a>
-    <a href="{{route('post.create')}}">建立文章</a>
-    <div class="container">
-        <div class="row">
-            @foreach($posts as $post)
-            <div class="col-7">
-                <h2>{{$post->title}}</h2>
-                <div>
-                    {{$post->content}}
-                </div>
-                <div>
-                    <a href="{{route('post.show',['post'=>$post->id])}}" class="btn btn-primary">繼續閱讀</a>
-                </div>
-                <hr>
+@extends('template.master')
+@section('main-title')
+    -- 文章列表
+@endsection
+@section('content')
+
+<h1>文章列表</h1>
+<a href="/post/create">建立文章</a>
+<a href="{{route('post.create')}}">建立文章</a>
+<div class="container">
+    <div class="row">
+        @foreach($posts as $post)
+        <div class="col-7">
+            <h2>{{$post->title}}</h2>
+            <div>
+                {{$post->content}}
             </div>
-            @endforeach
+            <div>
+                <a href="{{route('post.show',['post'=>$post->id])}}" class="btn btn-primary">繼續閱讀</a>
+            </div>
+            <hr>
         </div>
+        @endforeach
     </div>
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+</div>
+@endsection
--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>QQ BLOG</title>
+    <title>QQ BLOG  @yield('main-title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
-    <h1>Master</h1>
+    <nav>
+        <a href="#">建立文章</a>
+        <a href="#">建立分類</a>
+        <a href="#">建立商品</a>
+        <a href="#">註冊</a>
+        <a href="#">登入</a>
+    </nav>
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
