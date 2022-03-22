53c8a5e697507980e1f06cc55c8ffe3d6df8b971
Zac <a19860101@gmail.com>
2022-03-22 19:59:02 +0800

    20220322

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,20 +5,24 @@
         <div class="row justify-content-center">
             <div class="col-10 border rounded my-3 p-3">
                 <h2>{{$post->title}}</h2>
+                <div>
+                    <small>作者:{{$post->user->name}}</small>
+                </div>
                 <div class="my-3 lh-lg">
                     {!! $post->content !!}
                 </div>
                 <div class="my-3">
                     {{$post->updated_at}}
                 </div>
-                 <a href="{{route('post.index')}}" class="btn btn-primary">文章列表</a>
+                <a href="{{route('post.index')}}" class="btn btn-primary">文章列表</a>
+                @if(Auth::id() == $post->user_id)
                 <a href="{{route('post.edit',['post'=>$post->id])}}" class="btn btn-info">編輯</a>
                 <form action="{{route('post.destroy',['post'=>$post->id])}}" method="post" class="d-inline-block">
                     @csrf
                     @method('delete')
                     <input type="submit" class="btn btn-danger" value="刪除" onclick="return confirm('確認刪除？')">
                 </form>
-
+                @endif
             </div>
         </div>
     </div>
