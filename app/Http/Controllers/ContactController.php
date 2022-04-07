fbce0b3d57d1884b8a81cb084a5bf55b900d34e0
Zac <a19860101@gmail.com>
2022-04-07 21:59:44 +0800

    20220407

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Mail;
+// use TestMail;
 
 class ContactController extends Controller
 {
@@ -12,11 +13,13 @@ class ContactController extends Controller
         return view('contact.index');
     }
     function result(Request $request){
-        $mail = $request->mail;
-        Mail::raw('hello mail123',function($message){
-            $message->from('asdf@gmail.com');
-            $message->to('a19860101@gmail.com');
-            $message->subject('hello subject');
-        });
+        // $mail = $request->mail;
+        // Mail::raw('hello mail123',function($message){
+        //     $message->from('asdf@gmail.com');
+        //     $message->to('a19860101@gmail.com');
+        //     $message->subject('hello subject');
+        // });
+
+        Mail::to('a19860101@gmail.com')->send(new TestMail($request));
     }
 }
